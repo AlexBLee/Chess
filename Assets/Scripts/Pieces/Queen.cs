e72e1025491c8b3c7b3b12e6b2d2cafb2099ce4f
Alex Lee <alexanderbevislee@gmail.com>
2020-06-05 12:39:44 -0700

    Implemented movement for Queen and Rook

--- Assets/Scripts/Pieces/Queen.cs
+++ Assets/Scripts/Pieces/Queen.cs
@@ -4,5 +4,24 @@ using UnityEngine;
 
 public class Queen : Piece
 {
+    public override void FindLegalMoves()
+    {
+        for (int i = 1; i < 8; i++)
+        {
+            moves.Add(new Vector2(currentCoordinates.x - i, currentCoordinates.y));
+        }
 
+        for (int i = 1; i < 8; i++)
+        {
+            moves.Add(new Vector2(currentCoordinates.x + i, currentCoordinates.y));
+        }
+
+        for (int i = 1; i < 8; i++)
+        {
+            moves.Add(new Vector2(currentCoordinates.x, currentCoordinates.y + i));
+        }
+
+        moves.RemoveAll(tile => tile.x < 1 || tile.x > 8 || tile.y < 1 || tile.y > 8);
+
+    }
 }
