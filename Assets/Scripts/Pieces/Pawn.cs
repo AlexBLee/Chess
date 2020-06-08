7c4ecfe3d38c18b2290d5f69abb5f095d03e6230
Alex Lee <alexanderbevislee@gmail.com>
2020-06-07 18:42:57 -0700

    Fixed movement on pawn

--- Assets/Scripts/Pieces/Pawn.cs
+++ Assets/Scripts/Pieces/Pawn.cs
@@ -6,6 +6,7 @@ public class Pawn : Piece
 {
     bool firstMove = true;
 
+    // todo: this function is ugly and should be fixed
     public override void FindLegalMoves()
     {
         moves.Clear();
@@ -32,7 +33,29 @@ public class Pawn : Piece
         }
         else
         {
-            CalculateMoves(0, 1, true);
+            Vector2Int boardCoordPoint = 
+            new Vector2Int(currentCoordinates.x, currentCoordinates.y + (1 * forwardDirection));
+
+            if (!IsPieceAtTile(boardCoordPoint))
+            {
+                moves.Add(boardCoordPoint);
+            }
+
+            Vector2Int boardCoordPoint2 = 
+            new Vector2Int(currentCoordinates.x + 1, currentCoordinates.y + (1 * forwardDirection));
+
+            if (IsPieceAtTile(boardCoordPoint2) && IsEnemyPiece(boardCoordPoint2))
+            {
+                moves.Add(boardCoordPoint2);
+            }
+
+            Vector2Int boardCoordPoint3 = 
+            new Vector2Int(currentCoordinates.x - 1, currentCoordinates.y + (1 * forwardDirection));
+
+            if (IsPieceAtTile(boardCoordPoint3) && IsEnemyPiece(boardCoordPoint3))
+            {
+                moves.Add(boardCoordPoint3);
+            }
         }
         
     }
