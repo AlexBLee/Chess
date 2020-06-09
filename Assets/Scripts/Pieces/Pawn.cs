1e691269b987761d99f34e047f0da76240a75d3e
Alex Lee <alexanderbevislee@gmail.com>
2020-06-09 02:00:09 -0700

    Fixed pawn attacking wrong tiles

--- Assets/Scripts/Pieces/Pawn.cs
+++ Assets/Scripts/Pieces/Pawn.cs
@@ -6,14 +6,14 @@ public class Pawn : Piece
 {
     bool firstMove = true;
 
-    // todo: this function is ugly and should be fixed
     public override void FindLegalMoves()
     {
+        // Does not use the CalculateMove function because of the special movement patterns of the pawn.
+
         moves.Clear();
 
         if (firstMove)
         {
-            // Does not use the CalculateMove because of its first move edge case.
             for (int i = 1; i < 3; i++)
             {
                 Vector2Int boardCoordPoint = 
@@ -33,28 +33,20 @@ public class Pawn : Piece
         }
         else
         {
-            Vector2Int boardCoordPoint = 
-            new Vector2Int(currentCoordinates.x, currentCoordinates.y + (1 * forwardDirection));
-
-            if (!IsPieceAtTile(boardCoordPoint))
+            for (int i = -1; i < 2; i++)
             {
-                moves.Add(boardCoordPoint);
-            }
+                Vector2Int boardCoordPoint = 
+                new Vector2Int(currentCoordinates.x + i, currentCoordinates.y + (1 * forwardDirection));
 
-            Vector2Int boardCoordPoint2 = 
-            new Vector2Int(currentCoordinates.x + 1, currentCoordinates.y + (1 * forwardDirection));
+                if (i == 0 && !IsPieceAtTile(boardCoordPoint))
+                {
+                    moves.Add(boardCoordPoint);
+                }
+                else if (i != 0 && IsPieceAtTile(boardCoordPoint) && IsEnemyPiece(boardCoordPoint))
+                {
+                    moves.Add(boardCoordPoint);
+                }
 
-            if (IsPieceAtTile(boardCoordPoint2) && IsEnemyPiece(boardCoordPoint2))
-            {
-                moves.Add(boardCoordPoint2);
-            }
-
-            Vector2Int boardCoordPoint3 = 
-            new Vector2Int(currentCoordinates.x - 1, currentCoordinates.y + (1 * forwardDirection));
-
-            if (IsPieceAtTile(boardCoordPoint3) && IsEnemyPiece(boardCoordPoint3))
-            {
-                moves.Add(boardCoordPoint3);
             }
         }
         
