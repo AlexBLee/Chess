9d87f577b55a1aba054fdabc9975beee49af0dc1
Alex Lee <alexanderbevislee@gmail.com>
2020-06-27 17:28:57 -0700

    Uncluttered selection class

--- Assets/Scripts/Selection.cs
+++ Assets/Scripts/Selection.cs
@@ -7,7 +7,7 @@ public class Selection : MonoBehaviour
 {
     public Board board;
     public Piece selectedPiece;
-    public bool selecting;
+    public bool pieceSelected;
 
 
     void Update()
@@ -19,68 +19,83 @@ public class Selection : MonoBehaviour
         {
             if (Physics.Raycast(ray, out hit))
             {
-                if (selecting)
+                if (pieceSelected)
                 {
-                    Tile selectedTile = hit.transform.GetComponent<Tile>();
-
-                    // If the clicked tile is possible for the piece to move to, move there
-                    if (selectedPiece.moves.Any(move => move == selectedTile.coordinates))
-                    {
-                        // Destroy the piece at that tile
-                        if (selectedTile.piece != null)
-                        {
-                            Destroy(selectedTile.piece.gameObject);
-                        }
-
-                        // Colour the board back to normal
-                        board.ResetPieceMoveTileColours(selectedPiece);
-                        board.MovePieceTo(selectedPiece, selectedTile);
-                        
-                        GameManager.instance.SwitchSides();
-                        GameManager.instance.FindAllPossibleMoves();
-                        
-                        if (GameManager.instance.kingInCheck != null)
-                        {
-                            GameManager.instance.CheckKingCheck();
-                        }
-                        
-                    }
-                    else
-                    {
-                        Debug.Log(selectedTile.coordinates + " not in " + selectedPiece.name + " moves!"); 
-                        board.ResetPieceMoveTileColours(selectedPiece);
-                    }
-
-                    selecting = false;
+                    MakeMove(hit.transform.GetComponent<Tile>());
                 }
                 else
                 {
-                    selectedPiece = hit.transform.GetComponent<Tile>().piece;
-                    
-                    // Make sure the player is clicking the right piece
-                    if (selectedPiece != null && selectedPiece.interactable)
-                    {
-                        // Loop through each possible move that the piece can make
-                        foreach (Vector2Int move in selectedPiece.moves)
-                        {
-                            Piece piece = board.tiles[move.x][move.y].piece;
-
-                            // If the piece can attack a piece, then turn it red
-                            if (piece != null && (piece.interactable != selectedPiece.interactable))
-                            {
-                                board.SetTileColour(board.tiles[move.x][move.y], board.pieceAttack);
-                            }
-                            // Otherwise colour it normally
-                            else
-                            {
-                                board.SetTileColour(board.tiles[move.x][move.y], board.availableMoveColour);
-
-                            }
-                        }
-                        selecting = true;
-                    }
+                    SelectPiece(hit.transform.GetComponent<Tile>().piece);
                 }
             }
         }
+    }
+
+    public void SelectPiece(Piece piece)
+    {
+        if (piece != null && piece.interactable)
+        {
+            selectedPiece = piece;
+
+            // Loop through each possible move that the piece can make
+            foreach (Vector2Int move in selectedPiece.moves)
+            {
+                Tile tile = board.tiles[move.x][move.y];
+
+                // If the piece can attack a piece, then turn it red
+                if (tile.piece != null && (tile.piece.interactable != selectedPiece.interactable))
+                {
+                    board.SetTileColour(tile, board.pieceAttack);
+                }
+                // Otherwise colour it normally
+                else
+                {
+                    board.SetTileColour(tile, board.availableMoveColour);
+                }
+            }
+
+            pieceSelected = true;
+        }
+    }
+
+    public void MakeMove(Tile selectedTile)
+    {
+        // If the clicked tile is possible for the piece to move to, move there
+        if (selectedPiece.moves.Any(move => move == selectedTile.coordinates))
+        {
+            // Destroy the piece at that tile
+            if (selectedTile.piece != null)
+            {
+                // TODO: clear piece from board function
+                if (selectedTile.piece.render.sharedMaterial == board.pieceBlack)
+                {
+                    board.blackPieces.Remove(selectedTile.piece);
+                }
+                else
+                {
+                    board.whitePieces.Remove(selectedTile.piece);
+                }
+
+                Destroy(selectedTile.piece.gameObject);
+            }
+
+            // Colour the board back to normal
+            board.ResetPieceMoveTileColours(selectedPiece);
+            board.MovePieceTo(selectedPiece, selectedTile);
+            
+            GameManager.instance.SwitchSides();
+            GameManager.instance.FindAllPossibleMoves();
+            
+            if (GameManager.instance.kingInCheck != null)
+            {
+                GameManager.instance.CheckKingCheck();
+            }
+        }
+        else
+        {
+            board.ResetPieceMoveTileColours(selectedPiece);
+        }
+
+        pieceSelected = false;
     }
 }
