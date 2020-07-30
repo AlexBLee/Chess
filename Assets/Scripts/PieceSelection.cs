1cf1ccd28fc66fecd3578e1a1ea7c5ab7ad35ddf
Alex Lee <alexanderbevislee@gmail.com>
2020-07-30 01:13:10 -0700

    Snapped UI elements anchors properly

--- Assets/Scripts/PieceSelection.cs
+++ Assets/Scripts/PieceSelection.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class Selection : MonoBehaviour
+public class PieceSelection : MonoBehaviour
 {
     public Board board;
     public Piece selectedPiece;
