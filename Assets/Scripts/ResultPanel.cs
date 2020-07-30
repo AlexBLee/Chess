1cd37275d9eb8d0def6ae58a1d2385e0de07cd48
Alex Lee <alexanderbevislee@gmail.com>
2020-07-30 01:34:04 -0700

    Created a panel for end of game

---
+++ Assets/Scripts/ResultPanel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ResultPanel : MonoBehaviour
+{
+    TextMeshProUGUI resultText;
+    Button playAgainButton;
+    Button quitButton;
+
+    void Start()
+    {
+        playAgainButton.onClick.AddListener(PlayAgain);
+        quitButton.onClick.AddListener(QuitToMenu);
+    }
+
+    public void DisplayText(string text)
+    {
+        resultText.text = text;
+    }
+
+    void PlayAgain()
+    {
+
+    }
+
+    void QuitToMenu()
+    {
+
+    }
+}
