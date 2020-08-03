7ffaea0a595f6d9df765bbe634e6fbc54c89d1ed
Alex Lee <alexanderbevislee@gmail.com>
2020-08-02 18:07:05 -0700

    Added iTween plugin and added animations to moveTo

--- Assets/Scripts/Pieces/Pawn.cs
+++ Assets/Scripts/Pieces/Pawn.cs
@@ -111,7 +111,7 @@ public class Pawn : Piece
 
         // Move piece to new Tile
         tile.piece = this;
-        transform.position = tile.transform.position + new Vector3(0, 0.5f, 0);
+        iTween.MoveTo(gameObject, tile.transform.position + new Vector3(0, 0.5f, 0), 1.0f);
         location = tile.coordinates;
 
         CheckForMovementToPromotionTile(tile);
--- Assets/Scripts/Pieces/Piece.cs
+++ Assets/Scripts/Pieces/Piece.cs
@@ -136,7 +136,8 @@ public class Piece : MonoBehaviour
 
         // Move piece to new Tile
         tile.piece = this;
-        transform.position = tile.transform.position + new Vector3(0, 0.5f, 0);
+        // transform.position = tile.transform.position + new Vector3(0, 0.5f, 0);
+        iTween.MoveTo(gameObject, tile.transform.position + new Vector3(0, 0.5f, 0), 1.0f);
         location = tile.coordinates;          
     }
 
