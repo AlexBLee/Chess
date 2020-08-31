d8d7597fd117fb4b97241e0e6f6ecdb143ccb014
Alex Lee <alexanderbevislee@gmail.com>
2020-08-30 21:24:00 -0700

    Added getting timer values from PlayerPrefs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
@@ -18,6 +19,9 @@ public class MainMenu : MonoBehaviour
     public GameObject selectionPanel;
     public GameObject selectionPanel2;
 
+    public TMP_InputField timeField;
+    public TMP_InputField incField;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -30,6 +34,8 @@ public class MainMenu : MonoBehaviour
         whiteButton.onClick.AddListener(ChooseWhite);
         blackButton.onClick.AddListener(ChooseBlack);
         randomButton.onClick.AddListener(ChooseRandom);
+
+        GetPlayerPrefTimes();
     }
 
     public void PlayGame()
@@ -68,6 +74,12 @@ public class MainMenu : MonoBehaviour
         // The reason it's 0,2 rather than 0,1 is because the max value is not inclusive
         GameManager.whiteSide = (Random.Range(0,2) == 0) ? true : false;
         SceneManager.LoadScene("Main");
+    }
+
+    public void GetPlayerPrefTimes()
+    {
+        timeField.text = PlayerPrefs.GetInt("TimePerSide", 5).ToString();
+        incField.text = PlayerPrefs.GetInt("IncTime", 5).ToString();
     }
 
     public void SetTime(string value)
